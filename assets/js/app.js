9d82a3e11083ccd8794bcea0f5b1c6df935a3db2
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-31 19:21:33 +0200

    FIX active class binding

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,10 @@ Risposta dall'interlocutore: ad ogni inserimento di un messaggio, l'utente ricev
 
 Milestone 4
 Ricerca utenti: scrivendo qualcosa nell'input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo "mar" rimangono solo Marco e Martina)
+
+Milestone 5
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato.
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
 */
 
 const { createApp } = Vue
